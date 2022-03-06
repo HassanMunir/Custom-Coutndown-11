fe9a748a1da6a47e6bc5da22831944d37c6cb2e4
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2022-03-06 10:23:27 +0500

    Commit

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countDownDate = '';
+
 // Set Date Input minimum with Today's Date
-const today = new Date().toISOString().split('T')[0]; 
+const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take values from Form Input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countDownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countDownDate);
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
